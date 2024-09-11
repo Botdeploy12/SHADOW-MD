8483e2518f87cca8ae87f5c9e552e390c41631f6
Shadowmd99 <shadowwatsappbot@gmail.com>
2024-09-11 18:28:57 +0530

    Create restart.js

---
+++ plugins/restart.js
@@ -0,0 +1,22 @@
+const config = require('../config')
+const {cmd , commands} = require('../command')
+const {sleep} = require('../lib/functions')
+
+cmd({
+    pattern: "restartbot",
+    desc: "restart the bot QUEEN-KYLIE-MD",
+    category: "owner",
+    react: "👾",
+    filename: __filename
+},
+async(conn, mek, m,{from, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply}) => {
+try{
+const {exec} = require("child_process")
+reply("restarting shadow-md bot..")
+await sleep(1500)
+exec("pm2 restart all")
+}catch(e){
+console.log(e)
+reply(`${e}`)
+}
+})

e3924349ded26be680713f0a50dd9fbf0cd3cac6
Shadowmd99 <shadowwatsappbot@gmail.com>
2024-09-12 06:57:58 +0530

    Create anime.js

---
+++ plugins/anime.js
@@ -0,0 +1,22 @@
+const axios = require('axios');
+const { cmd, commands } = require('../command');
+
+cmd({
+    pattern: "animegirl",
+    desc: "Fetch a random anime girl image.",
+    category: "fun",
+    react: "👸🏻",
+    filename: __filename
+},
+async (conn, mek, m, { from, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply }) => {
+    try {
+        const apiUrl = `https://api.waifu.pics/sfw/waifu`;
+        const response = await axios.get(apiUrl);
+        const data = response.data;
+
+        await conn.sendMessage(from, { image: { url: data.url }, caption: '🧏🏻‍♀️ *Random Anime Girl Image* 🧏🏻‍♀️\n> > ❯❯ ꜱʜᴀᴅᴏᴡ-ᴍᴅ ᴡᴀᴛꜱ ᴀᴘᴘ ʙᴏᴛ ➣' }, { quoted: mek });
+    } catch (e) {
+        console.log(e);
+        reply(`*Error Fetching Anime Girl image*: ${e.message}`);
+    }
+});

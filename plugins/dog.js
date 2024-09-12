2040fbff2693f0f2677fa6124071a291061c468a
Shadowmd99 <shadowwatsappbot@gmail.com>
2024-09-12 06:59:37 +0530

    Create dog.js

---
+++ plugins/dog.js
@@ -0,0 +1,22 @@
+const axios = require('axios');
+const { cmd, commands } = require('../command');
+
+cmd({
+    pattern: "dog",
+    desc: "Fetch a random dog image.",
+    category: "fun",
+    react: "🐶",
+    filename: __filename
+},
+async (conn, mek, m, { from, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply }) => {
+    try {
+        const apiUrl = `https://dog.ceo/api/breeds/image/random`;
+        const response = await axios.get(apiUrl);
+        const data = response.data;
+
+        await conn.sendMessage(from, { image: { url: data.message }, caption: '🐶 *ʀᴀɴᴅᴏᴍ ᴅᴏɢ ɪᴍᴀɢᴇ* 🐶\n> ' }, { quoted: mek });
+    } catch (e) {
+        console.log(e);> ❯❯ ꜱʜᴀᴅᴏᴡ-ᴍᴅ ᴡᴀᴛꜱ ᴀᴘᴘ ʙᴏᴛ ➣
+        reply(`єяяσя ƒєт¢нιηg ∂σg ιмαgє: ${e.message}`);
+    }
+});

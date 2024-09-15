f6f7b3c655ff6a971fd3f834dbed841a9a07e66a
Lakshan Damayantha 💗➤ <shadowwatsappbot@gmail.com>
2024-09-15 18:58:24 +0530

    Update dog.js

--- plugins/dog.js
+++ plugins/dog.js
@@ -14,7 +14,7 @@ async (conn, mek, m, { from, quoted, body, isCmd, command, args, q, isGroup, sen
         const response = await axios.get(apiUrl);
         const data = response.data;
 
-        await conn.sendMessage(from, { image: { url: data.message }, caption: '🐶 *ʀᴀɴᴅᴏᴍ ᴅᴏɢ ɪᴍᴀɢᴇ* 🐶\n> *©Qᴜᴇᴇɴ ᴋʏʟɪᴇ-ᴍᴅ ʙʏ ꜱᴀʜᴀꜱ ᴛᴇᴄʜッ*' }, { quoted: mek });
+        await conn.sendMessage(from, { image: { url: data.message }, caption: '🐶 SHADOW MD BY OWNER🐶\n> *©Qᴜᴇᴇɴ ᴋʏʟɪᴇ-ᴍᴅ ʙʏ ꜱᴀʜᴀꜱ ᴛᴇᴄʜッ*' }, { quoted: mek });
     } catch (e) {
         console.log(e);
         reply(`єяяσя ƒєт¢нιηg ∂σg ιмαgє: ${e.message}`);

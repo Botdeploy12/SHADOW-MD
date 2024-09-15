ba789f6e3b2ed28de23700725aacfa2ae7556171
Lakshan Damayantha 💗➤ <shadowwatsappbot@gmail.com>
2024-09-15 11:29:52 +0530

    Update config.js

--- config.js
+++ config.js
@@ -6,7 +6,6 @@ function convertToBool(text, fault = 'true') {
 }
 module.exports = {
 SESSION_ID: process.env.SESSION_ID || "put sesson id",
-ALIVE_IMG: process.env.ALIVE_IMG || "https://telegra.ph/file/357b75726739a8b9c68de.jpg",
 SUDO_NB: process.env.SUDO_NB || "94767910958",
 AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "true",
 AUTO_BIO: process.env.AUTO_BIO || "true",

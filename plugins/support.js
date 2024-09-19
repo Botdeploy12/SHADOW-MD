4d35a067523afada6e340b85f3ec51715f7282b4
Lakshan Damayantha 💗➤ <shadowwatsappbot@gmail.com>
2024-09-19 09:23:43 +0530

    Create support.js

---
+++ plugins/support.js
@@ -0,0 +1,45 @@
+const config = require('../config')
+
+const {cmd , commands} = require('../command')
+
+
+
+cmd({
+
+    pattern: "support",
+
+    desc: "To get the bot informations.",
+
+    react: "👨‍🚀",
+
+    category: "main",
+
+    filename: __filename
+
+},
+
+async(conn, mek, m,{from, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply}) => {
+
+try{
+
+
+
+let about = ` *👋 Hello ${pushname}*
+
+*⚒️ SHADOW-MD Support Channels⚒️*
+
+*Whatsapp Channel Link:* https://whatsapp.com/channel/0029Vajn0gb8F2pFh7PDqO0c
+
+> ❯❯ ꜱʜᴀᴅᴏᴡ-ᴍᴅ ᴡᴀᴛꜱ ᴀᴘᴘ ʙᴏᴛ ➣`
+
+return await conn.sendMessage(from,{image: {url:`https://i.ibb.co/XLbkDd4/IMG-20240913-WA0048.jpg`},caption:about},{quoted: mek})
+
+}catch(e){
+
+console.log(e)
+
+reply(`${e}`)
+
+}
+
+})

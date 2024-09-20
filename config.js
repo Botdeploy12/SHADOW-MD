bd4dfd5ec5621df7a76ead28c1052b4008d15238
Botdeploy12 <lakshandamayantha964@gmail.com>
2024-09-20 15:03:51 +0530

    Update config.js

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "",
+SESSION_ID: process.env.SESSION_ID || "Rq1SWbJT#hfNIwYH9p0oS86muXxhJgWHFm7lvMW1xfSJtm50iOHE",
 SUDO_NB: process.env.SUDO_NB || "94767910958",
 AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "true",
 AUTO_BIO: process.env.AUTO_BIO || "true",

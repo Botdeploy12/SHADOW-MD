bcb19aed472aa9baeeea61ef2f095c6880cd9a10
Lakshan Damayantha 💗➤ <shadowwatsappbot@gmail.com>
2024-09-20 11:16:16 +0530

    Update support.js

--- plugins/support.js
+++ plugins/support.js
@@ -28,11 +28,11 @@ let about = ` *👋 Hello ${pushname}*
 
 *⚒️ SHADOW-MD Support Channels⚒️*
 
-*Whatsapp Channel Link:* https://whatsapp.com/channel/0029Vajn0gb8F2pFh7PDqO0c
+*Whatsapp Channel Link:* Follow the 𝐒𝐇𝐀𝐃𝐎𝐖 𝐌𝐃 channel on WhatsApp: https://whatsapp.com/channel/0029Vajn0gb8F2pFh7PDqO0c
 
 > ❯❯ ꜱʜᴀᴅᴏᴡ-ᴍᴅ ᴡᴀᴛꜱ ᴀᴘᴘ ʙᴏᴛ ➣`
 
-return await conn.sendMessage(from,{image: {url:`https://i.ibb.co/XLbkDd4/IMG-20240913-WA0048.jpg`},caption:about},{quoted: mek})
+return await conn.sendMessage(from,{image: {url:`https://telegra.ph/file/5f0335bebb60421970e8b.jpg`},caption:about},{quoted: mek})
 
 }catch(e){
 

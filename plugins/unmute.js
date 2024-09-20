c26e313927f5a58f5082d17492191a0691aece2c
Lakshan Damayantha 💗➤ <shadowwatsappbot@gmail.com>
2024-09-20 15:40:46 +0530

    Update unmute.js

--- plugins/unmute.js
+++ plugins/unmute.js
@@ -10,7 +10,9 @@ cmd({
 },
 async(conn, mek, m,{from, l, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants,  isItzcp, groupAdmins, isBotAdmins, isAdmins, reply}) => {
 try{
-         
+        if (!isGroup) return reply('This command can only be used in a group❗.')
+        if (!isBotAdmins) return reply('කරුනාකර ශැඩො md බොට් වෙත ඇඩ්මින් ලබා දෙන්න ❗.')
+        if (!isAdmins) return reply('ඔබ owner නොවෙ ❌.') 
   
             await conn.groupSettingUpdate(m.chat, "not_announcement")
            const mass = await conn.sendMessage(m.chat, { text: '*Group chat unmuted* 🔊' }, { quoted: mek });

436476c9e4f7a873697cacd216c93cf799e16d78
KEVIN-LEVIN-TECH <kevinathenlevin@gmail.com>
2024-12-11 06:51:31 +0530

    Update config.js

--- config.js
+++ config.js
@@ -5,8 +5,8 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "NiMATbrQ#HI5a-KItQ-B3VYhJTY3k2eNs-ss2dBWBDQAw0hcuJPM",
-SUDO_NB: process.env.SUDO_NB || "94767910958",
+SESSION_ID: process.env.SESSION_ID || "fN0QnaSZ#eJUc31aQhTsaVW8_xBQRwC47HNW2vov0oXCpYiHM3HI",
+SUDO_NB: process.env.SUDO_NB || "94784337506",
 AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "true",
 AUTO_BIO: process.env.AUTO_BIO || "true",
 MODE: process.env.MODE || "public",
